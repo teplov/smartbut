8b5f4041df124099d225926b6d06b37834b520d3
teplov <teplov@gmail.com>
2018-05-27 16:23:21 +0300

    port

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get('/a/:a/b/:b', function (req, res) {
     }
   });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
 });
